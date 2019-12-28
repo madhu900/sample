bc699b18d69be867060451483e9e34ff263f4b57
HP <HP@HP-PC>
2019-12-28 15:56:01 +0530

    iuunjkop,.

--- jdbcExamples/src/AddExpenseMain.java
+++ jdbcExamples/src/AddExpenseMain.java
@@ -15,7 +15,7 @@ public class AddExpenseMain {
 			connection = DriverManager.getConnection("jdbc:mysql://localhost:3308/Fer_jdbc", "root", "root");
 			statement = connection.prepareStatement(
 					"insert into expense (expensetype,date,price,noofitems,total,bywhom)values(?,?,?,?,?,?)");
-
+//statement("");
 			statement.setString(1, "type1");
 			statement.setString(2, "2019-11-26");
 			statement.setString(3, "1000");
@@ -26,6 +26,7 @@ public class AddExpenseMain {
 			//statement.addBatch();
 
 			int numberOfRecordInserted = statement.executeUpdate();
+			System.out.println("");
 			System.out.println("no of records inserted:" + numberOfRecordInserted);
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
